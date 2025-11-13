c714cf8dcbad11fadeacde384dc4610ed5197bed
Eneko Galan <enekogalanelorza@gmail.com>
2025-11-13 20:37:47 +0100

    Update to v2.3

--- Formula/logget.rb
+++ Formula/logget.rb
@@ -4,21 +4,21 @@
 class Logget < Formula
   desc "Command-line tool to extract browser logs and network data from web pages"
   homepage "https://github.com/enegalan/logget"
-  version "2.2"
+  version "2.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/enegalan/logget/releases/download/v2.2/logget-2.2-darwin-amd64.tar.gz"
-      sha256 "4d902201919e6a366542ac091d57672bd9fc1d3cce2e781e915cf9bfed573dcb"
+      url "https://github.com/enegalan/logget/releases/download/v2.3/logget-2.3-darwin-amd64.tar.gz"
+      sha256 "9ae51d194b41c09022990b79590be5f24e1c2ea10d9157f323bcb6bda668f743"
 
       def install
         bin.install "logget-darwin-amd64" => "logget"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/enegalan/logget/releases/download/v2.2/logget-2.2-darwin-arm64.tar.gz"
-      sha256 "c881cae801fb8f201ce42838a5edf141b581b397b2aa8a4226b892b636df9e3c"
+      url "https://github.com/enegalan/logget/releases/download/v2.3/logget-2.3-darwin-arm64.tar.gz"
+      sha256 "3e2018529c44a62b39a0a6dea54b285bdb1da852ebfa384d3731dd03e3e534c9"
 
       def install
         bin.install "logget-darwin-arm64" => "logget"
